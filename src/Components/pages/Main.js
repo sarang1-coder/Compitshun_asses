00c364d2b1b58cde45d4ed399b38a5c5ea348ce5
LinkedLst <sarang15mar@gmail.com>
2023-11-03 16:05:43 +0530

    main

---
+++ src/Components/pages/Main.js
@@ -0,0 +1,18 @@
+import React,{useEffect} from 'react'
+
+
+
+
+
+const Main = () => {
+
+    const[name,setName]=React.useState("");
+
+
+  return (
+    <h1><i>Todo List App</i></h1>
+
+  )
+}
+
+export default Main
